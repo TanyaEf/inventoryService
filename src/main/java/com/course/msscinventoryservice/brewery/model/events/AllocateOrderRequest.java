6746c3bc061d32cdbd6596f03cff7eb871a493a5
TanyaEf <efimenkot@gmail.com>
2020-01-29 18:09:09 +0200

    handle allocation request

---
+++ src/main/java/com/course/msscinventoryservice/brewery/model/events/AllocateOrderRequest.java
@@ -0,0 +1,16 @@
+package com.course.msscinventoryservice.brewery.model.events;
+
+import com.course.msscinventoryservice.brewery.model.BeerOrderDto;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class AllocateOrderRequest {
+    private BeerOrderDto beerOrderDto;
+}
+
